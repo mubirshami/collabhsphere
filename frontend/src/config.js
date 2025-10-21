acff23730656d5b0d9fb21a8b3df785ec9aded12
mubirshami <mubirshami2000@gmail.com>
2025-10-21 15:37:52 +0500

    ENV Variables

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,5 +1,5 @@
 // API Configuration
-const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000/api';
-const SOCKET_URL = process.env.REACT_APP_SOCKET_URL || 'http://localhost:5000';
+const API_URL = process.env.REACT_APP_API_URL;
+const SOCKET_URL = process.env.REACT_APP_SOCKET_URL;
 
 export { API_URL, SOCKET_URL };
